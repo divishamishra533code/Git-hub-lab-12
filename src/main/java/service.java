50d23df43d511c728588d7ac36c516e3cdcff0db
divishamishra533code <divishamishra533@gmail.com>
2026-04-09 10:01:39 +0530

    Initial commit

---
+++ src/main/java/service.java
@@ -0,0 +1,37 @@
+package com.example.backend.service;
+
+import com.example.backend.entity.Student;
+import com.example.backend.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repo;
+
+    public StudentService(StudentRepository repo) {
+        this.repo = repo;
+    }
+
+    public Student save(Student student) {
+        return repo.save(student);
+    }
+
+    public List<Student> getAll() {
+        return repo.findAll();
+    }
+
+    public Student update(Long id, Student s) {
+        Student existing = repo.findById(id).orElseThrow();
+        existing.setName(s.getName());
+        existing.setEmail(s.getEmail());
+        existing.setCourse(s.getCourse());
+        return repo.save(existing);
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+    }
+}
